bcb3009d50552c9541306f0a46483f1fe12c257a
Abdullahi Odesanmi <56407386+realabdullah@users.noreply.github.com>
2021-08-25 21:05:38 +0100

    modal

--- src/components/Tracker.vue
+++ src/components/Tracker.vue
@@ -3,21 +3,24 @@
     <h2>IP Address Tracker</h2>
     <div class="input">
       <input type="text" v-model="searchip" placeholder="Search for any IP address or domain">
-      <img src="../assets/images/icon-arrow.svg" alt="icon">
+      <a href="#modal" role="button" >
+        <img src="../assets/images/icon-arrow.svg" alt="icon">
+      </a>
     </div>
 
-    <div class="modal">
+    <div class="modal" id="modal">
       <span>Ip Address</span>
-      <p>192.212.174.101</p>
+      <p>Oops!</p>
 
       <span>Location</span>
-      <p>Brooklyn</p>
+      <p>I'm not</p>
 
       <span>Timezone</span>
-      <p>UTC-05:00</p>
+      <p>Functional yet!</p>
 
       <span>Isp</span>
-      <p>SpaceX Starlink</p>
+      <p>Check Back later</p>
+      <a href="#!" class="outside-trigger"></a>
     </div>
   </div>
   <div class="map-area">
@@ -28,9 +31,7 @@
 <script>
 export default {
   name: 'Tracker',
-
-  setup() {
-  }
+  
 }
 </script>
 
@@ -65,6 +66,10 @@ input::placeholder {
   color: hsl(0, 0%, 17%);
   font-weight: bold;
 }
+a {
+  text-decoration: none;
+  margin-top: 5px;
+}
 img {
   background: #000;
   padding: 16px;
@@ -80,6 +85,15 @@ img {
   background: #fff;
   border-radius: 10px;
 }
+#modal {
+	opacity: 0;
+	transition: .5s ease-in-out;
+	visibility: hidden;
+}
+#modal:target {
+  opacity: 1;
+  visibility: visible;
+}
 .modal span {
   text-transform: uppercase;
   font-size: 10px;
@@ -93,8 +107,18 @@ img {
   font-weight: 500;
   font-size: 18px;
 }
+.outside-trigger {
+	bottom: 0;
+	cursor: default;
+	left: 0;
+	position: fixed;
+	right: 0;
+	top: 0;
+}
 .map-area {
   min-height: 50vh;
   background: url('../assets/images/map.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 </style>
